b08fedcd99deca3f329e8f2ea4bc3eb35680e931
liaoxx <993501756@qq.com>
2020-05-02 16:27:24 +0800

    2020-05-02 切换到数据库 3306 端口

--- application/database.php
+++ application/database.php
@@ -23,9 +23,9 @@ return [
     // 用户名
     'username'        => Env::get('database.username', 'root'),
     // 密码
-    'password'        => Env::get('database.password', 'liao123456'),
+    'password'        => Env::get('database.password', 'root'),
     // 端口
-    'hostport'        => Env::get('database.hostport', '33061'),
+    'hostport'        => Env::get('database.hostport', '3306'),
     // 连接dsn
     'dsn'             => '',
     // 数据库连接参数
