5dfb15950d96ecacfa2679724f5994209fb11b99
liaoxx <993501756@qq.com>
2020-05-02 16:24:40 +0800

    2020-05-02 恢复部分 被忽略的文件

--- thinkphp/library/think/db/Connection.php
+++ thinkphp/library/think/db/Connection.php
@@ -292,6 +292,7 @@ abstract class Connection
                 if ($config['debug']) {
                     $startTime = microtime(true);
                 }
+                //dump($config);die;
                 $this->links[$linkNum] = new PDO($config['dsn'], $config['username'], $config['password'], $params);
                 if ($config['debug']) {
                     // 记录数据库连接信息
